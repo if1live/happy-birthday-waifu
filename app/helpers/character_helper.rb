7010c495765ea702f4660b86215d096089714181
if1live <libsora25@gmail.com>
2015-01-28 15:20:14 +0900

    month/day <-> string

--- app/helpers/character_helper.rb
+++ app/helpers/character_helper.rb
@@ -1,2 +1,17 @@
 module CharacterHelper
+  def self.date_to_s(month, day)
+    return nil if month > 12
+    return nil if month < 1
+    return nil if day > 31
+    return nil if day < 1
+
+    Kernel::sprintf("%02d/%02d", month, day)
+  end
+
+  def self.str_to_date(val)
+    return {} unless val.length == 5
+    month = val[0...2].to_i
+    day = val[3...5].to_i
+    {month: month, day: day}
+  end
 end
--- spec/helpers/character_helper_spec.rb
+++ spec/helpers/character_helper_spec.rb
@@ -13,3 +13,31 @@ require 'rails_helper'
 #RSpec.describe CharacterHelper, :type => :helper do
 #  pending "add some examples to (or delete) #{__FILE__}"
 #end
+
+describe CharacterHelper do
+  describe '#date_to_s' do
+    it 'convert day/month to string' do
+      month = 2
+      day = 5
+      actual = CharacterHelper.date_to_s month, day
+      expected = '02/05'
+      expect(actual).to eq(expected)
+    end
+
+    it 'invalid month/day' do
+      expect(CharacterHelper.date_to_s 0, 1).to be_nil
+      expect(CharacterHelper.date_to_s 13, 1).to be_nil
+
+      expect(CharacterHelper.date_to_s 1, 32).to be_nil
+      expect(CharacterHelper.date_to_s 1, 0).to be_nil
+    end
+  end
+
+  describe '#str_to_date' do
+    it 'convert string to monty/day' do
+      actual = CharacterHelper.str_to_date '02/05'
+      expected = {month: 2, day: 5}
+      expect(actual).to be_eql(expected)
+    end
+  end
+end
