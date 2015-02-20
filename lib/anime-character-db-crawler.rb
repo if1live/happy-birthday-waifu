a6153fa1bfba650399f2c1332d46a7dbae0f1d90
if1live <libsora25@gmail.com>
2015-02-21 01:19:01 +0900

    read profile, image, thumbnail

--- lib/anime-character-db-crawler.rb
+++ lib/anime-character-db-crawler.rb
@@ -1,52 +1,96 @@
 require 'nokogiri'
 
 module AnimeCharacterDB
-=begin
-=end
-
   class CharacterMetadata
+    # required
     attr_accessor :character_id,
                   :name_en,
                   :name_jp,
-                  :aliases,
-                  :role,
+                  :birthday,
+                  :aliases
+
+    # predefined
+    attr_accessor :role,
                   :tags,
                   :cv,
                   :voice_actors,
                   :type,
-                  :height,
-                  :blood_type,
-                  :birthday
+                  :blood_type
+
+    # image
+    attr_accessor :thumbnail,
+                  :image
+
+    attr_reader :extra
+
+    def initialize()
+      @extra = {}
+    end
+
     def character_id=(val)
       @character_id = val.to_i
     end
 
-    def tags=(val)
+    def filter_to_array(val)
       if val.is_a? String
-        @tags = val.split ', '
+        val.split ', '
       else
-        @tags = val
+        val
       end
     end
 
+    def tags=(val)
+      @tags = filter_to_array(val)
+    end
+
     def voice_actors=(val)
-      if val.is_a? String
-        @voice_actors = val.split ', '
-      else
-        @voice_actors = val
+      @voice_actors = filter_to_array(val)
+    end
+
+    def birthday=(val)
+      month_table = {
+        january: 1,
+        february: 2,
+        march: 3,
+        april: 4,
+        may: 5,
+        june: 6,
+        july: 7,
+        august: 8,
+        september: 9,
+        october: 10,
+        november: 11,
+        december: 12
+      }
+      unless val.nil?
+        token_list = val.split(' ')
+        raise ArgumentError.new "unknown date format" unless token_list.length == 2
+
+        month_key = token_list[0].downcase.to_sym
+        month = month_table[month_key]
+        raise ArgumentError.new "unknwon month, #{month_key}" if month.nil?
+
+        day = token_list[1].to_i
+
+        @birthday = sprintf("%02d/%02d", month, day)
       end
     end
+
+    def set_extra(key, value)
+      @extra[key] = value
+    end
   end
+
 
   class CharacterHTMLParser
     def initialize()
       @doc = nil
     end
 
-    def get_profile_table(doc)
+    def get_content_table(doc, header)
       h3_list = doc.css('h3')
       h3_list.each do |elem|
-        if elem.inner_text == 'Profile'
+        if elem.inner_text == header
           # find property table
           curr = elem.next_sibling
           while not curr.nil? do
@@ -60,8 +104,29 @@ module AnimeCharacterDB
           end
         end
       end
+
     end
 
+    def get_profile_table(doc)
+      get_content_table doc, 'Profile'
+    end
+
+    def filter_profile_key(key)
+      key = key.gsub '(more)', ''
+      key = key.downcase
+      key = key.strip
+      key = key.gsub ' ', '_'
+      key.to_sym
+    end
+
+    def filter_profile_value(value)
+      value = value.strip
+      if value == ''
+        nil
+      else
+        value
+      end
+    end
 
     def read html
       doc = Nokogiri::HTML.parse(html) do |config|
@@ -69,34 +134,108 @@ module AnimeCharacterDB
       end
 
       metadata = CharacterMetadata.new
+      read_profile doc, metadata
+      read_thumbnail doc, metadata
+      read_image doc, metadata
 
+      metadata
+    end
+
+    def read_thumbnail(doc, metadata)
+      img_node = doc.css('img#b0:not(.vector)')[0]
+      src = img_node.get_attribute :src
+      metadata.thumbnail = src
+      metadata
+    end
+
+    def read_image(doc, metadata)
+      img_node_list = doc.css('img')
+      img_node_list.each do |el|
+        src = el.get_attribute :src
+        unless src.include? 'ami.animecharactersdatabase.com'
+          next
+        end
+
+        id_val = el.get_attribute :id
+        next unless id_val.nil?
+
+        invalid_text_array = [
+          '/FLAGS/',
+          '/emote/',
+          '/forum/',
+          '/img/hearts/',
+          '/thumbs/',
+          '/guild/',
+          '/productimages/',
+          '/bg/',
+          '/48x48/',
+          '/rss2.png',
+          '/img/vote.png',
+          '/hosted/Red2.png',
+          '/PWA_Award.png',
+        ]
+
+        is_valid = true
+        invalid_text_array.each do |text|
+          if src.include?(text)
+            is_valid = false
+            break
+          end
+        end
+
+        next unless is_valid
+
+        raise ArgumentError.new "cannot find valid character image url" unless metadata.image.nil?
+        metadata.image = src
+      end
+      metadata
+    end
+
+    def read_profile(doc, metadata)
       table = get_profile_table doc
       tr_list = table.css('tr')
       tr_list.each do |tr_node|
         th_node = tr_node.css('th')[0]
         td_node = tr_node.css('td')[0]
 
-        key = th_node.inner_text.strip
-        value = td_node.inner_text.strip
+        key = filter_profile_key th_node.inner_text
+        value = filter_profile_value td_node.inner_text
         #puts "#{key} = #{value}"
 
-        cond_table = {
-          'Character ID' => 'character_id',
-          'Romaji Name' => 'name_en',
-          'Japanese Name' => 'name_jp',
-          'Aliases' => 'aliases',
-          'Role' => 'role',
-          'Tagged' => 'tags',
-          'CV' => 'cv',
-          'Voice Actors' => 'voice_actors',
-          'Type' => 'type',
-          'Height' => 'height',
-          'Blood Type' => 'blood_type',
-          'Birthday (more)' => 'birthday',
+        # 실제로 서비스에서 사용할 확률이 높은 데이터
+        required_cond_table = {
+          character_id: 'character_id',
+          romaji_name: 'name_en',
+          japanese_name: 'name_jp',
+          aliases: 'aliases',
+          birthday: 'birthday',
         }
 
-        attr_name = cond_table[key]
-        metadata.send("#{attr_name}=", value)
+        # 데이터에 있을거라고 기대하지만 실제로 안쓸거같은 데이터
+        predefined_cond_table = {
+          role: 'role',
+          tagged: 'tags',
+          cv: 'cv',
+          voice_actors: 'voice_actors',
+          type: 'type',
+          blood_type: 'blood_type',
+          #height: 'height',
+          #bust: 'bust',
+          #waist: 'waist',
+          #hip: 'hip',
+        }
+
+        if required_cond_table.include? key
+          attr_name = required_cond_table[key]
+          metadata.send("#{attr_name}=", value)
+
+        elsif predefined_cond_table.include? key
+          attr_name = predefined_cond_table[key]
+          metadata.send("#{attr_name}=", value)
+
+        else
+          metadata.set_extra(key, value)
+        end
       end
       metadata
     end
--- spec/lib/anime-character-db-crawler/anime-character-db-crawler_spec.rb
+++ spec/lib/anime-character-db-crawler/anime-character-db-crawler_spec.rb
@@ -10,25 +10,97 @@ def get_filepath(filename)
   file_path = File.join(html_dir, filename)
   return file_path
 end
-describe AnimeCharacterDB::CharacterHTMLParser do
-  let(:filepath) { get_filepath 'character-13162-kasugano-sora.html' }
-  let(:html) { File.read filepath }
-  let(:parser) { AnimeCharacterDB::CharacterHTMLParser.new }
-  describe '#read' do
+
+describe AnimeCharacterDB::CharacterMetadata do
+  let(:subject) { AnimeCharacterDB::CharacterMetadata.new }
+  describe '#birthday=' do
     it 'success' do
-      data = parser.read html
+      subject.birthday = 'February 5'
+      expect(subject.birthday).to eq('02/05')
+    end
+  end
+end
+
+describe AnimeCharacterDB::CharacterHTMLParser do
+  let(:kasugano_sora_filepath) { get_filepath 'character-13162-kasugano-sora.html' }
+  let(:kasugano_sora_html) { File.read kasugano_sora_filepath }
+
+  let(:ibuki_shirakusa_filepath) { get_filepath 'character-62894-ibuki-shirakusa.html' }
+  let(:ibuki_shirakusa_html) { File.read ibuki_shirakusa_filepath }
+
+  let(:parser) { AnimeCharacterDB::CharacterHTMLParser.new }
+
+  describe '#read' do
+    it 'kasugano sora' do
+      data = parser.read kasugano_sora_html
+
+      # required
       expect(data.character_id).to be(13162)
       expect(data.name_en).to eq('Sora Kasugano')
       expect(data.name_jp).to eq('春日野 穹 （かすがの そら）')
-      expect(data.aliases).to eq('')
+      expect(data.aliases).to be_nil
+      expect(data.birthday).to eq('02/05')
+
+      # predefined
       expect(data.role).to eq('Main Character')
       expect(data.tags).to eq(['loli', 'ribbon', 'lolita', 'hair ribbons', 'dress', 'doll'])
       expect(data.cv).to eq('白波遥')
       expect(data.voice_actors).to eq(['Hiroko Taguchi', '田口宏子', 'たぐち ひろこ'])
       expect(data.type).to eq('Normal')
-      expect(data.height).to eq('152cm')
       expect(data.blood_type).to eq('O')
-      expect(data.birthday).to eq('February 5')
+
+      # extra
+      expect(data.extra[:height]).to eq('152cm')
+
+      # image
+      thumbnail = 'http://ami.animecharactersdatabase.com/./images/yosuganosora/Sora_Kasugano_thumb.jpg'
+      expect(data.thumbnail).to eq(thumbnail)
+
+      image = 'http://ami.animecharactersdatabase.com/./images/yosuganosora/Sora_Kasugano.png'
+      expect(data.image).to eq(image)
+    end
+
+    it 'ibuki shirakursa' do
+      data = parser.read ibuki_shirakusa_html
+      # required
+      expect(data.character_id).to eq(62894)
+      expect(data.name_en).to eq('Ibuki Shirakusa')
+      expect(data.name_jp).to eq('白草 いぶき （しらくさ いぶき）')
+      expect(data.aliases).to be_nil
+      expect(data.birthday).to be_nil
+
+      # predefined
+      expect(data.role).to eq('Unsorted')
+      expect(data.tags).to eq(['v bangs', 'ribbon', 'stockings', 'wavy hair', 'school uniform', 'hairband', 'twin'])
+      expect(data.cv).to eq('藤咲ウサ')
+      expect(data.type).to eq('Normal')
+
+      # extra
+      expect(data.extra[:height]).to eq('152cm')
+      expect(data.extra[:bust]).to eq('79cm')
+      expect(data.extra[:waist]).to eq('57cm')
+      expect(data.extra[:hip]).to eq('83cm')
+
+      # image
+      thumbnail = 'http://ami.animecharactersdatabase.com/uploads/chars/thumbs/200/11498-135469648.jpg'
+      expect(data.thumbnail).to eq(thumbnail)
+
+      image = 'http://ami.animecharactersdatabase.com/uploads/chars/11498-1811138177.jpg'
+      expect(data.image).to eq(image)
+    end
+  end
+
+  describe '#filter_profile_key' do
+    it 'Birthday (more)' do
+      key = 'Birthday (more)'
+      actual = parser.filter_profile_key key
+      expect(actual).to eq(:birthday)
+    end
+
+    it 'Blood Type' do
+      key = 'Blood Type'
+      actual = parser.filter_profile_key key
+      expect(actual).to eq(:blood_type)
     end
   end
 end
