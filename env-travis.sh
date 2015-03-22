64489e26492d2c2c1af9dcbba0b85976f1dee484
if1live <libsora25@gmail.com>
2015-03-22 12:37:38 +0900

    travis 에서 환경 변수 없으면 죽길래 스크립트 수정

---
+++ env-travis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# copy from "bundle exec rake secret"
+export SECRET_KEY_BASE="GENERATE-SECRET"
+
+export TWITTER_APP_KEY="CREATE-TWITTER-APP"
+export TWITTER_APP_SECRET="CREATE-TWITTER-APP"
+
+export TWITTER_ACCESS_TOKEN="ADMIN-TOKEN"
+export TWITTER_ACCESS_TOKEN_SECRET="ADMIN-TOKEN-SECRET"
+
+export ADMIN_PASSWORD="pw"
+
+export MYSQL_DATABASE="db"
+export MYSQL_USERNAME="id"
+export MYSQL_PASSWORD="pw"
+
