31e878081e6d5e0618d94f876151db2d95f37134
if1live <libsora25@gmail.com>
2015-04-02 22:08:40 +0900

    add sidekiq

---
+++ app/workers/birthday_bot_worker.rb
@@ -0,0 +1,33 @@
+require 'date'
+
+class BirthdayBotWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    access_token = ENV['TWITTER_ACCESS_TOKEN']
+    secret_token = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+
+    bot = BirthdayBot::TwitterSender.create access_token, secret_token, false
+
+    #date = Date.today
+    date = Date.new 2015, 4, 3
+    today_str = Character.date_to_s date.month, date.day
+    characters = Character.where(date: today_str)
+    characters.each do |character|
+      tweet = bot.tweet_character_birthday character
+      retval = { ok: true, url: tweet.uri.to_s }
+      puts retval
+    end
+  end
+end
+
+class HealthCheckWorker
+  include Sidekiq::Worker
+
+  def perform(*args)
+    msg = "Health Check : #{Time.now}"
+
+    logger.info msg
+    puts msg
+  end
+end
--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
 source env.sh
-RAILS_ENV=production bundle exec unicorn -c config/unicorn.rb -D
 
+function setup_server {
+	RAILS_ENV=production bundle exec rake db:migrate
+	RAILS_ENV=production bundle exec rake db:seed
+	RAILS_ENV=production bundle exec rake assets:precompile
+}
+
+function run_server {
+	RAILS_ENV=production bundle exec unicorn -c config/unicorn.rb -D
+}
+
+function kill_server {
+	PID=$(ps aux|grep "unicorn master"|grep "\-D"|awk '{ print $2}')
+	kill $PID
+}
+
+function kill_sidekiq {
+	PID=$(ps aux|grep "sidekiq"|grep "happy"|awk '{ print $2}')
+	kill $PID
+}
+
+function run_sidekiq {
+	bundle exec sidekiq -C config/sidekiq.yml -e production -d
+}
+
+setup_server
+kill_server
+run_server
+
+kill_sidekiq
+run_sidekiq
