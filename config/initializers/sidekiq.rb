fe490ca5d729d2fdf5d24b63d1c5a03fbb64bda0
if1live <libsora25@gmail.com>
2015-04-02 21:39:58 +0900

    sidekiq http basic auth

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,12 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+# http://stackoverflow.com/questions/12265421/sidekiq-authentication
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [Rails.application.secrets.user, Rails.application.secrets.password]
+end
+
+
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file)
