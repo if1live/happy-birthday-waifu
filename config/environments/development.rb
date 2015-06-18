558723c3707c4013077bd03b64a1de47cd999755
if1live <libsora25@gmail.com>
2015-06-18 11:32:56 -0400

    support redis to go

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+  # https://devcenter.heroku.com/articles/redistogo
+  ENV["REDISTOGO_URL"] = 'redis://127.0.0.1:6379'
 end
