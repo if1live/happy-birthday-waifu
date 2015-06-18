768f07f7c9977800621c6439d28d2b9a639b92ea
if1live <libsora25@gmail.com>
2015-06-18 12:16:42 -0400

    redis address

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,6 +7,12 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
 end
 
 Sidekiq.configure_server do |config|
+  Rails.logger = Sidekiq::Logging.logger
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+  config.redis = { url: uri }
+end
+
+Sidekiq.configure_client do |config|
   uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
   config.redis = { url: uri }
 end
